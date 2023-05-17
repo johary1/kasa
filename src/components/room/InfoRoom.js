c94193af5e3b8a5b5cd4e84094ce9642152c37e3
johary1 <joharisoa@gmail.com>
2023-05-17 12:15:08 +0200

    update info-room and responsive for logo

--- src/components/room/InfoRoom.js
+++ src/components/room/InfoRoom.js
@@ -9,29 +9,30 @@ const InfoRoom = ({ room }) => {
     <div className="info-room">
       <div className="info-room-title-location">
         <h2 className="info-room-title">{title}</h2>
-        <p className="info-room-location">{location}</p>
+        <h4 className="info-room-location">{location}</h4>
       </div>
-      <ul className="info-room-tags">
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-      <div className="info-room-rating-host">
-        <div className="info-room-rating">
-          {[...Array(maxRating)].map((_, i) => (
-            <img
-              key={i}
-              src={i < rating ? "/filled-star.png" : "/empty-star.png"}
-              alt={i < rating ? "Filled star" : "Empty star"}
-              className="info-room-rating-star"
-            />
+      <div className="info-room-tags">
+        <ul>
+          {tags.map((tag) => (
+            <li key={tag}>{tag}</li>
           ))}
-        </div>
+        </ul>
+      </div>
 
-        <div className="info-room-host">
-          <img className="info-room-host-picture" src={picture} alt={name} />
-          <p className="info-room-host-name">{name}</p>
-        </div>
+      <div className="info-room-rating">
+        {[...Array(maxRating)].map((_, i) => (
+          <img
+            key={i}
+            src={i < rating ? "/filled-star.png" : "/empty-star.png"}
+            alt={i < rating ? "Filled star" : "Empty star"}
+            className="info-room-rating-star"
+          />
+        ))}
+      </div>
+
+      <div className="info-room-host">
+        <span className="info-room-host-name">{name}</span>
+        <img className="info-room-host-picture" src={picture} alt={name} />
       </div>
     </div>
   );
