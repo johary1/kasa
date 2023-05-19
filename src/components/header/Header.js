5e3939c800de5b5b45e9517ec6021586def0926c
johary1 <joharisoa@gmail.com>
2023-05-19 09:55:45 +0200

    add link to logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Logo from "../logo/Logo";
+import { Link } from "react-router-dom";
 import Navigation from "../navigation/Navigation";
 
 const headerColor = "#ff6060";
 const Header = () => {
   return (
     <div className="header">
-      <Logo color={headerColor} imgSrc="/favicon.png" />
+      <Link to="/">
+        <Logo color={headerColor} imgSrc="/favicon.png" />
+      </Link>
       <Navigation />
     </div>
   );
