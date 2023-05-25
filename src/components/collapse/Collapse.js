9cef5228c99cd9a59acf1fff825b63055880da64
johary1 <joharisoa@gmail.com>
2023-05-25 10:39:32 +0200

    manage collaspse state with eventlistener for outside click

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Collapse = ({ label, description, width }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const collapsibleRef = useRef(null);
+
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        collapsibleRef.current &&
+        !collapsibleRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="collapsible" style={{ width }}>
+    <div className="collapsible" style={{ width }} ref={collapsibleRef}>
       <div className="collapsible-header">
         <div className="collapsible-label">{label}</div>
         <button
